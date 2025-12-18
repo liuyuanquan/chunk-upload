6f65f0121c8a17949ac60d224f403530b916a72c
liuyuanquan <522703297@qq.com>
2025-12-19 00:27:35 +0800

    fix: 恢复vite配置，添加publicDir以支持测试文件访问

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-	root: ".",
+	root: "./demo",
 	server: {
 		open: true,
-		port: 3000,
 	},
 	build: {
-		outDir: "dist-demo",
+		outDir: "../dist-demo",
 	},
 	resolve: {
 		alias: {
 			"@xumi/chunk-upload-lib": resolve(__dirname, "./src"),
 		},
 	},
+	publicDir: "../",
 });
