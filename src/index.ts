d1448e493b869c920d8584176495276925e2f697
liuyuanquan <522703297@qq.com>
2025-12-19 08:32:08 +0800

    refactor: 移除不需要的内部实现导出，仅保留公开 API

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,5 @@
 export * from './types'
 export { chunkUpload } from './chunkUpload'
 export { chunkUploadStream } from './chunkUploadStream'
-export { chunkFile } from './chunkFile'
 export { clearHashCache, getCacheSize } from './createChunk'
-export { getWorkerPool, resetWorkerPool, WorkerPool } from './utils/workerPool'
-export { validateFile } from './utils/fileValidator'
-export { createCancelController } from './utils/cancelController'
-export { withRetry } from './utils/retry'
-export {
-	calculateChunkStrategy,
-	calculateWorkerCount,
-} from './utils/chunkStrategy'
-export { calculateHash, shouldUseOptimizedHash } from './utils/hashOptimizer'
-export { getWorkerUrl, createWorker } from './utils/workerUrl'
+export { getWorkerPool, resetWorkerPool } from './utils/workerPool'
