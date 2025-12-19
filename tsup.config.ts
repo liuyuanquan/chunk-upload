21648dace307a543af3e54ef26464239adabc5cc
刘源泉 <522703297@qq.com>
2025-12-19 12:03:54 +0800

    refactor: 移除 CommonJS 格式，仅保留 ESM 格式

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: {
-    index: 'src/index.ts',
-    work: 'src/work.ts',
+    index: "src/index.ts",
+    work: "src/work.ts",
   },
-  format: ['esm', 'cjs'],
+  format: ["esm"],
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
   treeshake: true,
-})
+});
